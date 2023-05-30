6b5206aa1998a4342b77e026997ae2689ac3fcda
Karol Cyganik <karol.cyganik@wp.pl>
2023-05-30 12:46:14 +0200

    add a dummy onnx model to test the framework

---
+++ src/random_not_trained_cnn_model.py
@@ -0,0 +1,33 @@
+from typing import Any
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class SimpleCNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(2704, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 2)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+#to onnx with imput 224x224
+dummy_input = torch.randn(1, 3, 64, 64)
+
+model = SimpleCNN()
+# predict dummy input
+output = model(dummy_input)
+print(output)
+
+torch.onnx.export(model, dummy_input, "random_not_trained_cnn.onnx", verbose=True)
