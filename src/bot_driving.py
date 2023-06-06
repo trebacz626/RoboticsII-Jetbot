250373fc9e39b16efaf33949e565a84aa979ff68
Kacper Trebacz <trebacz.kacper@gmail.com>
2023-06-06 13:27:45 +0200

    tricks

--- src/bot_driving.py
+++ src/bot_driving.py
@@ -27,16 +27,24 @@ def main():
 
     input('Robot is ready to ride. Press Enter to start...')
 
-    forward, left = 0.0, 0.0
+    BUFFER_SIZE = 3
+    forward, left = [0] * BUFFER_SIZE, [0] * BUFFER_SIZE
     while True:
-        print(f'Forward: {forward:.4f}\tLeft: {left:.4f}')
-        driver.update(forward, left)
+        print(f'Forward: {forward[0]:.4f}\tLeft: {left[0]:.4f}')
+        driver.update(forward[0], left[0])
 
         ret, image = video_capture.read()
         if not ret:
             print(f'No camera')
             break
-        forward, left = ai.predict(image)
+        new_forward, new_left = ai.predict(image)
+        if new_left < 0 and new_left > -0.45:
+            new_left = 0
+        new_left = 0.6 * new_left + 0.4 * left[-1]
+        left.append(new_left)
+        forward.append(new_forward)
+        left = left[1:]
+        forward = forward[1:]
 
 
 if __name__ == '__main__':
